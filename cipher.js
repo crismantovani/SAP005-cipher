04e7aead6d51fe9508e4d7b9db98a9f9a10da464
crismantovani <crismantovani87@gmail.com>
2020-11-05 22:00:23 -0300

    Arquivos

--- cipher.js
+++ cipher.js
@@ -1,5 +1,13 @@
 const cipher = {
-  // ...
+  encode(offsetNum, inputStr){
+    let menscriP = ""
+    console.log(encode) 
+  }
+
+  decode(offsetNum, inputStr){
+    let mensdecriP = ""
+    console.log(decode)
+
 };
 
 export default cipher;
