ebad164da6210655d88cb7a0d2cf1b367f312723
crismantovani <crismantovani87@gmail.com>
2020-11-06 01:02:31 -0300

    Criando Arrays no cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,26 @@
-const cipher = {
-  encode(offsetNum, inputStr){
-    let menscriP = ""
-    console.log(encode) 
-  }
 
-  decode(offsetNum, inputStr){
-    let mensdecriP = ""
-    console.log(decode)
+const alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-};
+array.lenght = 25;
+if (valorPosicaoOriginal + valorOffset > array.length){
 
-export default cipher;
+let diferenca = array.lenght - (valorPosicaoOriginal + valorOffset);
+     novaLetra = array[diferenca];
+}
+
+
+
+
+
+
+
+
+// A lógica é:
+    // Pegar o que foi digitado;
+        //Pegar o offser;
+        //Iniciar o loop dentro do que foi digitado, começando na 1a letra;
+        //Somar o resultado e ver se é maior q o tamanho do alfabeto; Caso ñ, soma a posicao+offset
+        //Acha a novaPosicao e imprime ela 
+        //Se for maior que o alfabeto
+        //Calcula o valor que passará e imprime a partir de zero o novo valor
+        //Fecha o loop q reinicia na 2a letra até terminar de passar em todas
